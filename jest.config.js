c695cd75441f95af99cc990b9dc58af178e04770
AndreiKaliasinskiIndy <andrei.kaliasinski@weareindy.com>
2021-05-07 12:26:06 +0300

    Added repository

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    verbose: true,
+    preset: "jest-playwright-preset",
+    testMatch: ["**/?(*.)+(spec|test).+(js)"],
+    maxWorkers: 1,
+    testEnvironmentOptions:{
+    "jest-playwright":{
+        browsers: ["chromium"/*, "firefox", "webkit"*/],
+        exitOnPageError: false, 
+        launchOptions: {
+          headless: false,
+          slowMo: 1000
+        }
+     }
+    },
+    "reporters": [
+        "default",
+        ["jest-html-reporters", {
+            "publicPath": "./html-report",
+            "filename": "report.html"
+        }]
+      ]
+};
---
+++ tests/SmokeTests/LoginAndRegistration/UserCanLogin.spec.js
@@ -0,0 +1,13 @@
+jest.setTimeout(30000);
+jest.retryTimes(1);
+const {LoginPage} = require ("../../../Pages/LoginPage");
+const testData = require("../../../TestData/TestData.json");
+const {WorkSpacePage} = require ("../../../Pages/WorkSpacePage");
+
+it("Should login", async () => {
+    const loginPage = new LoginPage(page);
+    await loginPage.openLoginPage();
+    await loginPage.login(testData.users.user1.Email, testData.users.user1.Password);
+    const workSpacePage = new WorkSpacePage(page);
+    await workSpacePage.clickMenuItem("Home");
+});
